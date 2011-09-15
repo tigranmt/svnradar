e2419f55244ae678c4d2ab56036e692d80c804f5
TigranMartirosyan <TigranMartirosyan@localhost>
2011-09-15 23:02:37 +0200

    Fix on styling of tooltip control

--- RepoManager/FormatConverters/RichTextFormatter.cs
+++ RepoManager/FormatConverters/RichTextFormatter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Windows.Controls;
 using System.Windows.Documents;
+using System.Text.RegularExpressions;
 
 namespace SvnRadar.FormatConverters
 {
@@ -15,8 +16,21 @@ namespace SvnRadar.FormatConverters
         }
 
         public void SetText(System.Windows.Documents.FlowDocument document, string text)
-        {
-            new TextRange(document.ContentStart, document.ContentEnd).Text = text + System.Environment.NewLine;
+        {            
+            Regex regx = new Regex("http://([\\w+?\\.\\w+])+([a-zA-Z0-9\\~\\!\\@\\#\\$\\%\\^\\&amp;\\*\\(\\)_\\-\\=\\+\\\\\\/\\?\\.\\:\\;\\'\\,]*)?", RegexOptions.IgnoreCase);
+            MatchCollection col = regx.Matches(text);
+
+
+            //if (col != null && col.Count > 0)
+            //{
+            //    foreach (Match match in col)
+            //    {
+            //        new TextRange(document.ContentStart, document.ContentEnd).ApplyPropertyValue( = text.Substring()
+            //    }
+            //}
+            //else
+             new TextRange(document.ContentStart, document.ContentEnd).Text = text;
+
         }
     }
 }
